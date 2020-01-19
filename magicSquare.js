e21d69dbf7e419b2376540f6d1b5ccde0504aa93
amarsingh95 <54304507+amarsingh95@users.noreply.github.com>
2020-01-19 12:21:54 +0530

    Add files via upload

---
+++ magicSquare.js
@@ -0,0 +1,54 @@
+function MagicSquare(n) {
+    let arr = [];
+    let count = 1;
+    for (let l = 0; l < n; l++) {
+        arr[l] = [];
+    }
+
+    for (let l = 0; l < n; l++) {
+        for (let m = 0; m < n; m++) {
+            arr[l][m] = 0;
+
+        }
+
+    }
+
+
+    let i = parseInt(n / 2);
+    let j = n - 1;
+
+    for (let num = 0; num <= n * n; num++) {
+
+        if (i == -1 && j == n) {
+            i = 0;
+            j = n - 2;
+        } else {
+            if (i == -1) {
+                i = n - 1;
+            }
+            if (j == n) {
+                j = 0;
+            }
+        }
+
+
+
+        if (arr[i][j] == 0) {
+            arr[i][j] = count;
+            i = i - 1;
+            j = j + 1;
+            count = count + 1;
+
+        } else {
+            i = i + 1;
+            j = j - 2;
+        }
+
+    }
+
+
+
+    console.log(arr);
+}
+var MatrixNum = 4;
+MagicSquare(MatrixNum);
